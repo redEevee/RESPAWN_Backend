7379a700550c2f141500feb1ee176fc30b46f01a
jiwon <kjw042626@gmail.com>
2025-07-05 19:09:51 +0900

    test-2

--- src/main/java/com/shop/respawn/Test.java
+++ src/main/java/com/shop/respawn/Test.java
@@ -1,6 +1,15 @@
 package com.shop.respawn;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Test {
 
     int a;
+
+    public Test(int a) {
+        this.a = a;
+    }
 }
