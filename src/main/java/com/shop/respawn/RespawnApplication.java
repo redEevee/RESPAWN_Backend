23ada66ceb9e268192f0370642ff7e2a558d7130
jiwon <kjw042626@gmail.com>
2025-07-05 18:17:51 +0900

    project setup

---
+++ src/main/java/com/shop/respawn/RespawnApplication.java
@@ -0,0 +1,13 @@
+package com.shop.respawn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespawnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespawnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/respawn/RespawnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.respawn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespawnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
