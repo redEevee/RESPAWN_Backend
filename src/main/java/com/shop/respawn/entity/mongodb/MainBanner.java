3bf1bea576654390f051f8023ae02416df95304d
jiwon <kjw042626@gmail.com>
2025-07-10 23:02:25 +0900

    MongoDB 추가 및 연결 Thymeleaf 추가 이미지 DB에 저장 및 가져와서 보여주기 간단한 html 작성

---
+++ src/main/java/com/shop/respawn/entity/mongodb/MainBanner.java
@@ -0,0 +1,23 @@
+package com.shop.respawn.entity.mongodb;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter @Setter
+@Document(collection = "main-banner")
+@AllArgsConstructor
+@NoArgsConstructor
+public class MainBanner {
+
+    @Id @GeneratedValue
+    private String id;
+    private String imageFileId; // GridFS에 저장된 이미지의 fileId
+    private String title;
+
+}
