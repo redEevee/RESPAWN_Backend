365395c4dabb4a2065bb120ab39f85d8cc478779
jiwon <kjw042626@gmail.com>
2025-07-18 23:14:02 +0900

    Feat: 이메일 발송 코드 추가

---
+++ src/main/java/com/shop/respawn/email/EmailAuthResponseDto.java
@@ -0,0 +1,14 @@
+package com.shop.respawn.email;
+
+import lombok.Getter;
+
+@Getter
+public class EmailAuthResponseDto {
+    private boolean success;
+    private String responseMessage;
+
+    public EmailAuthResponseDto(boolean success, String responseMessage){
+        this.success = success;
+        this.responseMessage = responseMessage;
+    }
+}
---
+++ src/main/java/com/shop/respawn/email/EmailController.java
@@ -0,0 +1,24 @@
+package com.shop.respawn.email;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/email")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    // 인증번호 전송
+    @GetMapping("/auth")
+    public EmailAuthResponseDto sendAuthCode(@RequestParam String address) {
+        return emailService.sendEmail(address);
+    }
+
+    // 인증번호 검증
+    @PostMapping("/auth")
+    public EmailAuthResponseDto checkAuthCode(@RequestParam String address, @RequestParam String authCode) {
+        return emailService.validateAuthCode(address, authCode);
+    }
+}
