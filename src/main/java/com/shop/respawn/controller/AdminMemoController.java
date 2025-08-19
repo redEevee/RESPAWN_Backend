12d760c9ab4a154c80be47cde7b8806e2ee01f9b
jiwon <kjw042626@gmail.com>
2025-08-19 22:37:40 +0900

    Refactor: 관리자가 유저에 대한 메모 기능 수정

--- src/main/java/com/shop/respawn/controller/AdminMemoController.java
+++ src/main/java/com/shop/respawn/controller/AdminMemoController.java
@@ -4,11 +4,13 @@ import com.shop.respawn.dto.adminMemo.AdminMemoResponse;
 import com.shop.respawn.dto.adminMemo.AdminMemoUpsertRequest;
 import com.shop.respawn.service.AdminMemoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/memo")
 @RequiredArgsConstructor
+@Secured("ROLE_ADMIN")
 public class AdminMemoController {
 
     private final AdminMemoService memoService;
