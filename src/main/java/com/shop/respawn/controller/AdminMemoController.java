7f3744f7581d1ef532c8e4ce95b60aedb92e5454
jiwon <kjw042626@gmail.com>
2025-08-19 22:29:49 +0900

    Feat: 관리자가 유저에 대한 메모 기능 추가

---
+++ src/main/java/com/shop/respawn/controller/AdminMemoController.java
@@ -0,0 +1,30 @@
+package com.shop.respawn.controller;
+
+import com.shop.respawn.dto.adminMemo.AdminMemoResponse;
+import com.shop.respawn.dto.adminMemo.AdminMemoUpsertRequest;
+import com.shop.respawn.service.AdminMemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/memo")
+@RequiredArgsConstructor
+public class AdminMemoController {
+
+    private final AdminMemoService memoService;
+
+    // 사용자당 1개: 없으면 생성, 있으면 수정
+    @PostMapping("/upsert")
+    public AdminMemoResponse upsert(@RequestBody AdminMemoUpsertRequest request) {
+        return memoService.upsert(request);
+    }
+
+    // 단일 사용자 메모 조회
+    @GetMapping
+    public AdminMemoResponse get(
+            @RequestParam String userType,
+            @RequestParam Long userId
+    ) {
+        return memoService.getByUser(userType, userId);
+    }
+}
